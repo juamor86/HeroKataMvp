359455e31ee0de496343b447ff310dfa21372dd1
Juan Mora <juan.mora@1eeeurope.de>
2018-01-16 12:22:12 +0100

    later on more commit

--- app/src/main/java/eeurope/herokatamvp/ui/splash/SplashActivity.java
+++ app/src/main/java/eeurope/herokatamvp/ui/splash/SplashActivity.java
@@ -1,4 +1,4 @@
-package eeurope.herokatamvp.splash;
+package eeurope.herokatamvp.ui.ui.splash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- app/src/main/java/eeurope/herokatamvp/ui/splash/SplashPresenter.java
+++ app/src/main/java/eeurope/herokatamvp/ui/splash/SplashPresenter.java
@@ -1,4 +1,4 @@
-package eeurope.herokatamvp.splash;
+package eeurope.herokatamvp.ui.ui.splash;
 
 /**
  * Created by Mora on 08/09/2017.
